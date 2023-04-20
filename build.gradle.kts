a683c4342c1b025e8e6a9ae87efd05b8fc18937f
Aiko Vandeputte <aiko@aikovdp.me>
2023-04-20 22:51:39 +0200

    Initial setup

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    java
+}
+
+group = "com.rokucraft"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+tasks {
+    processResources {
+        val props = "version" to version
+        inputs.properties(props)
+        filteringCharset = "UTF-8"
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+    }
+}
---
+++ src/main/java/com/rokucraft/rokuhorses/RokuHorses.java
@@ -0,0 +1,17 @@
+package com.rokucraft.rokuhorses;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RokuHorses extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
