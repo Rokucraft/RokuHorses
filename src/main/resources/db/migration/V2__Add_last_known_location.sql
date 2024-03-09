5c081c2738abe31b60d8cd9d1ed36fc24b6b5037
Aiko Vandeputte <aiko@aikovdp.me>
2024-03-09 12:45:12 +0100

    Remove V2 migration

--- src/main/resources/db/migration/V2__Add_last_known_location.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE horse
-    ADD last_known_x TEXT;
-ALTER TABLE horse
-    ADD last_known_y TEXT;
-ALTER TABLE horse
-    ADD last_known_z TEXT;
-ALTER TABLE horse
-    ADD last_known_world TEXT;
